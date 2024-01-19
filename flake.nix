74df5c085218db07671e8223f75f9a0ca6afb3b0
Sylvain Carisey <sylvain@carisey.dev>
2024-01-19 09:18:17 +0100

    build(flake.nix)

--- flake.nix
+++ flake.nix
@@ -5,13 +5,24 @@
   };
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: {
+            jdk = final.graalvmCEPackages.graalvm-ce-musl;
+            jre = final.graalvmCEPackages.graalvm-ce-musl;
+          })
+        ];
+      };
     in
+    with pkgs;
     {
       devShells.default = pkgs.mkShell {
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
         packages = with pkgs; [ sbt scalafmt graalvmCEPackages.graalvm-ce-musl ];
         shellHook = ''
           export JAVA_HOME=${pkgs.graalvmCEPackages.graalvm-ce-musl}
+          export NATIVE_IMAGE_INSTALLED=true
         '';
       };
     }
